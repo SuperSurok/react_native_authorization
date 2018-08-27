2f2d3a2cf54b0732fb9ab0efa51f8bd87c27473e
Sergey Lebedev <oddyone@mail.ru>
2018-08-27 23:57:23 +0300

    Install Firebase library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Header} from './components/common'
+import firebase from 'firebase';
 
 class App extends Component {
-    render(){
-        return(
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCf5OoWq4g2noIp2Zv_HZvjWB9iWhs4rqw',
+            authDomain: 'authentication-212da.firebaseapp.com',
+            databaseURL: 'https://authentication-212da.firebaseio.com',
+            projectId: 'authentication-212da',
+            storageBucket: '',
+            messagingSenderId: '39888032208'
+        });
+    }
+
+    render() {
+        return (
             <View>
                 <Header headerText="Authentication"></Header>
                 <Text>An app</Text>
