00b84b0a27bf515e956f0fb0a2728603abc7a327
Sergey Lebedev <oddyone@mail.ru>
2018-08-28 00:25:07 +0300

    Fix components export

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
-import {Header} from './components/common'
+import {Header} from './common/index'
 import firebase from 'firebase';
 
 class App extends Component {
