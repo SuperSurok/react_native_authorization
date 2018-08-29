96002314c1c1c37472cf6b21e5481202961b1420
Sergey Lebedev <oddyone@mail.ru>
2018-08-29 22:34:28 +0300

    Create Input Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Header} from './common/index'
 import firebase from 'firebase';
-import LoginForm from './components/LoginForm';
+import LoginForm from './LoginForm';
 
 class App extends Component {
     componentWillMount() {
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,30 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {TextInput} from 'react-native';
 import {Button, Card, CardSection} from "./common";
 
 class LoginForm extends Component {
-    render(){
-        return(
+    state = {text: ''};
+
+    render() {
+        return (
             <Card>
+                <CardSection>
+                    <TextInput
+                        value={this.state.text}
+                        onChangeText={text => this.setState({text})}
+                        style={{height: 20, width: 100}}/>
+                </CardSection>
+
                 <CardSection/>
-                <CardSection/>
+
                 <CardSection>
                     <Button>
                         Log In
                     </Button>
                 </CardSection>
             </Card>
-        )
-
+        );
+    }
 }
 
 export default LoginForm;
